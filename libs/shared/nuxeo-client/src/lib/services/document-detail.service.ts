56fdc13602a5f993ef760b84be4d73169460363a
Narasimha Koppula <narasimha.koppula@hyland.com>
2026-04-09 16:51:12 +0530

    save collection fix

--- libs/shared/nuxeo-client/src/lib/services/document-detail.service.ts
+++ libs/shared/nuxeo-client/src/lib/services/document-detail.service.ts
@@ -269,11 +269,9 @@ export class DocumentDetailService {
   }
 
   createCollection(title: string, description = ''): Observable<NuxeoDocument> {
-    return this.api.post<NuxeoDocument>(`/nuxeo/api/v1/path/@userWorkspace`, {
-      'entity-type': 'document',
-      type: 'Collection',
-      name: title.replace(/\s+/g, '-').toLowerCase(),
-      properties: { 'dc:title': title, 'dc:description': description },
+    return this.api.post<NuxeoDocument>('/nuxeo/api/v1/automation/Collection.Create', {
+      params: { name: title, description },
+      context: {},
     });
   }
 
